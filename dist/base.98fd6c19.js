f64e6eef5de86630aabc1986a283885cb23daa01
Jessica Maple <maplejessbeth@gmail.com>
2021-02-04 13:16:25 -0500

    update CSS

--- dist/base.98fd6c19.js
+++ dist/base.98fd6c19.js
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59465" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63056" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
